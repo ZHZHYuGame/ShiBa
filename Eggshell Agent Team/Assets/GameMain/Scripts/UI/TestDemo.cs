66a330ea2ab4a9c0d24550ad0308e7806ed9a816
wangrui <142725407@qq.com>
2025-02-26 13:37:42 +0800

    123

---
+++ Eggshell Agent Team/Assets/GameMain/Scripts/UI/TestDemo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("超大猪");
+        Debug.Log("是梁浩冉");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
