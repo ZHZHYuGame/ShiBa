22951b61e2f00ae4cbf4f767ab78599c0661ca12
彭泽义 <2657757226@qq,com>
2025-02-27 19:55:40 +0800

    2.27作业

--- Eggshell Agent Team/Assets/GameFramework/Scripts/ILRuntime/AppDomalinLanch.cs
+++ Eggshell Agent Team/Assets/GameFramework/Scripts/ILRuntime/AppDomalinLanch.cs
@@ -40,8 +40,7 @@ public class AppDomalinLanch : UnitySingleto<AppDomalinLanch>
         }
         //PDB文件是调试数据库，如需要在日志中显示报错的行号，则必须提供PDB文件，不过由于会额外耗用内存，正式发布时请将
         InitializeILRuntime();//初始化
-       MyFist myFist=new MyFist();
-        myFist.OnRuan(appdomain);
+ 
         
         MyTest myTest = new MyTest();
         myTest.OnRun(appdomain); 
