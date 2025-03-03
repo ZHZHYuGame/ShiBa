1c8b4639b7b28bcb7409640dd36bd32b2cf1cc38
HXN <2284933085@qq.com>
2025-03-03 08:34:11 +0800

    胡鑫楠20250303提交编辑器

---
+++ Eggshell Agent Team/Assets/Editor/EnemyEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnemyEditor : EditorWindow
+{
+    [MenuItem("StageTool/玩家编辑器")]
+    public static void Init()
+    {
+        GetWindow<EnemyEditor>().Show();
+    }
+    private void OnGUI()
+    {
+        
+    }
+}
