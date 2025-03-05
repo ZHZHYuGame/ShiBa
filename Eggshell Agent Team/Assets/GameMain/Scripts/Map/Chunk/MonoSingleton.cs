e4ffc3507731985072071dc40ee21c7753d29ca8
知彗 <b17384624390@163.com>
2025-03-05 17:28:43 +0800

    修复了因为脚本缺失导致的报错

---
+++ Eggshell Agent Team/Assets/GameMain/Scripts/Map/Chunk/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if(instance!=null)
+                return instance;
+            instance = FindObjectOfType(typeof(T)) as T;
+            if(instance!=null)
+            return instance;
+            instance = new GameObject("_"+typeof(T).Name).AddComponent<T>();
+            return instance;
+        }
+    }
+}
