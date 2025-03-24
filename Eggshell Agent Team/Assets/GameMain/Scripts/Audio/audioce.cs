4948d887888fd4e355b33df6a51fa2539df432cb
njh <jyzzy25402@163.com>
2025-03-24 20:06:45 +0800

    1

--- Eggshell Agent Team/Assets/GameMain/Scripts/Audio/audioce.cs
+++ Eggshell Agent Team/Assets/GameMain/Scripts/Audio/audioce.cs
@@ -7,7 +7,7 @@ public class audioce : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,107 +26,11 @@ public class audioce : MonoBehaviour
             AudioMgr.Instance.PlaySFX(clip);
             AudioMgr.Instance.PlayMusic(clip);
 
-             }
-            if (Input.GetKeyDown(KeyCode.D))
+        }
+        if (Input.GetKeyDown(KeyCode.D))
         {
             AudioClip clip = Resources.Load<AudioClip>("music_about");
-            //AudioMgr.OnAudioFilterRead();
 
-
-
-
-            //#pragma warning disable format
-            //#pragma warning disable format
-            //fd
-            //ReturnToPoolWhenFinishedfds
-            //ReturnToPoolWhenFinishedfdsfds
-            //finishedfdsfdsgfdsag
-            //finishedfdsfdsgfdsaggds
-            //gainfdsg
-            //finishedfdsfdsgfdsaggdsgdsfdsagdsagfdsgfdsgds
-            //gdsfdsdgsfgdsgsfdgfd
-            //gfd
-            //fgd
-            //gfds
-            //gdfs
-            //gds
-            //gds
-            //gdsg
-            //defaultSpatialBlendgdsdsgdsgsd
-            //gds
-            //gs
-            //dsg
-            //gds
-            //gdsfdsdgsfgdsgds
-            //finishedfdsfdsgfdsaggdsf
-            //gdsfdsdgsfgdsgdsgdsgds
-            //sgd
-            //gdsfdsdgsfgdsgdsgfds
-            //gdsfdsdgsfgdsgsfdgfdgds
-            //gdsfdsdgsfgdsgdsgfdsgfdsgfds
-            //gfsd
-            //h
-            //fgdhfd
-            //GetKeyDownewg
-            //gfds
-            //afds
-            //gdsfdsdgsfhgdf
-            //hgfd
-            //gdsfdsdgsfgdssgd
-            //gfds
-            //gfds
-            //gdfs
-            //gds
-
-            //gdsfdsdgsfgdsgdsgfdsgfdsgfdsgfds
-            //gdsfdsdgsfgdsgdsgfdsgfds
-            //gds
-            //fgds
-            //gds
-            //finishedfdsfdsgfdsaggdsfgdsa
-            //fgds
-            //gfsda
-            //gfdsa
-            //gdfs
-            //g
-            //dsgfgds
-            //finishedfdsfdsgfdsaggdsgdsfdsagdsagfdsgfdsgdsdfds
-
-            //finishedfdsfdsgfdsaggdsgdsfdsagdsagfdsgfdsgdsgsd
-            //gdsfdsdgsfgdsgdsgfdsgfdsgfdsdgs
-            //gainfdsggdsfdgs
-            //gdsfdsdgsfgdsgdsgfdsgfdsgfdsgfdsew
-            //ew
-            //gr
-            //g
-            //reservedfds
-            //ReturnToPoolWhenFinishedgr
-            //hgfd
-            //gfds
-            //fdsg
-
-            
-            //gainfdsggds
-            //gds
-            //gds
-            //finishedfdsfdsgfdsaggdsgsd
-            //gainareg
-            //defaultSpatialBlendgds
-            //gainfdsggdsgds
-            //gdsg
-            //defaultSpatialBlendgdssg
-            //defaultSpatialBlendgdsdsgds
-            //g
-            //defaultSpatialBlendgdsdgs
-            //gainfdsggdsgdsfds
-            //finishedfdsfdsgfdsagfds
-            //gainfdsggdsgdsfdsds
-            //gdsfdsdgsf
-            //dgsf
-            //gdsfdsdgsfgds
-
-            //#pragma warning restore format
-            //#pragma warning restore format
         }
 
     }
