4be5941a21522dbfbcafd2464e2a465d581e4778
wangrui <142725407@qq.com>
2025-03-28 10:00:36 +0800

    副本功能模块开发

---
+++ 王瑞/BossAnimalCellViews.cs
@@ -0,0 +1,49 @@
+using EnhancedUI.EnhancedScroller;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossAnimalCellViews : EnhancedScrollerCellView
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Text names;
+    [SerializeField] private GameObject black,locks;
+    [SerializeField] private Text level;
+    [SerializeField] private Text tiems;
+    
+    internal void SetData(BossData bossData)
+    {
+        //设置toggle组
+        gameObject.GetComponent<Toggle>().group = transform.parent.parent.GetComponent<ToggleGroup>();
+        icon.sprite = Resources.Load<Sprite>("BossView/boss_item_" + bossData.icon);
+        names.text = bossData.name;
+        if (bossData.status != 0)
+        {
+            black.gameObject.SetActive(true);
+            locks.gameObject.SetActive(true);
+            // 将Unix时间戳转换为DateTimeOffset
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(bossData.time);
+
+            // 获取UTC时间
+            DateTime utcTime = dateTimeOffset.UtcDateTime;
+
+            // 转换为本地时间
+            DateTime localTime = dateTimeOffset.ToLocalTime().DateTime;
+            TimeSpan timeSpan = localTime - DateTime.Now;
+            tiems.text = $"{timeSpan.Hours}:{timeSpan.Minutes}:{timeSpan.Seconds}";
+        }
+        else
+        {
+            black.gameObject.SetActive(false);
+            locks.gameObject.SetActive(false);
+            tiems.text = "<color=#00931F>已刷新</color>";
+        }
+        level.text = $"Lv.{bossData.level}";
+    }
+    private void Start()
+    {
+        
+    }
+}
---
+++ 王瑞/BossScrollerController.cs
@@ -0,0 +1,49 @@
+using EnhancedUI.EnhancedScroller;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class BossScrollerController : MonoBehaviour, IEnhancedScrollerDelegate
+{
+    [SerializeField]
+    EnhancedScroller enhancedScroller;
+    [SerializeField]
+    BossAnimalCellViews animalCellViewPerfab;//预制体
+    List<BossData> datas;
+    void Start()
+    {
+        datas = new List<BossData>();
+        datas.Add(new BossData( "褒姒·国威",101,0,390, 1739761514));
+        datas.Add(new BossData("钟无艳·国威", 102,0,420, 1739761514));
+        datas.Add(new BossData("蒙恬·国威", 104,0,450, 1739761514));
+        datas.Add(new BossData("落魄将军·国威", 105,0,480, 1739761514));
+        datas.Add(new BossData("山大王·国威", 116,1,500, 1739803922));
+        enhancedScroller.Delegate = this;
+        enhancedScroller.ReloadData();//数据刷新
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public EnhancedScrollerCellView GetCellView(EnhancedScroller scroller, int dataIndex, int cellIndex)
+    {
+        BossAnimalCellViews animalCellView = scroller.GetCellView(animalCellViewPerfab) as BossAnimalCellViews;
+        animalCellView.SetData(datas[dataIndex]);
+        return animalCellView;
+    }
+
+    public float GetCellViewSize(EnhancedScroller scroller, int dataIndex)
+    {
+        return 160f;//大小
+    }
+
+    public int GetNumberOfCells(EnhancedScroller scroller)
+    {
+        return datas.Count;//返回Item的数量。
+    }
+    
+}
