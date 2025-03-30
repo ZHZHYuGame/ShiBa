083386c20991a7bdd6989a9a82d9cb33d16556f7
ZhangHR <3150984297@qq.com>
2025-03-30 20:27:36 +0800

    111

---
+++ 张浩然/MessageManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 月考一服务器
+{
+    internal class MessageManager<T> : Singleton<MessageManager<T>>
+    {
+        Dictionary<int, Action<T>> MesDic = new Dictionary<int, Action<T>>();
+        public void AddListen(int id, Action<T> action)
+        {
+            if (MesDic.ContainsKey(id))
+            {
+                MesDic[id] += action;
+            }
+            else
+            {
+
+                MesDic.Add(id, action);
+            }
+        }
+        public void OnBoardCast(int id, T t)
+        {
+            if (MesDic.ContainsKey(id))
+            {
+                MesDic[id](t);
+            }
+        }
+    }
+}
